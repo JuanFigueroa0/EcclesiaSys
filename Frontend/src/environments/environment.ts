e23aff6ab8328f4fc40277c0c3ef672657bc4bd7
JuanFigueroa0 <152662886+JuanFigueroa0@users.noreply.github.com>
2026-07-20 21:11:44 -0500

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ecclesia-sys-backend-orcin.vercel.app/api/v1/auth/login',
-  // Configuración Auth0 (rellenar con tus valores)
-  auth0Domain: '',
-  auth0ClientId: '',
-  auth0Audience: '',
+  apiUrl: 'https://ecclesia-sys-backend-orcin.vercel.app/api/v1/',
 };
